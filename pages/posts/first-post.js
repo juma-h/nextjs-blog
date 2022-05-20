ff0263de63752add314b1d5f465d19b91ec4d77f
Juma-h <mitchfibi@gmail.com>
2022-05-20 10:35:44 +0300

    added working project

---
+++ pages/posts/first-post.js
@@ -0,0 +1,24 @@
+import Script from 'next/script';
+import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../../components/layout';
+
+export default function FirstPost(){
+    return (
+        <>
+        <Head> 
+           <title>First Post </title>
+         
+           
+        </Head>
+
+        <h1>First Post</h1>
+
+        <h2>
+            <Link href="/">
+            <a>Back to Home</a>
+            </Link>
+        </h2>
+        </>
+    );
+}
